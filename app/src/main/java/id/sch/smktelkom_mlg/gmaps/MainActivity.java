8f69cddf744aea9c0d96945fbe2ef0dc4e8c78b8
arnettarahma <arnettarahma@gmail.com>
2017-10-22 00:36:03 +0700

    Street view hide name

--- app/src/main/java/id/sch/smktelkom_mlg/gmaps/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/gmaps/MainActivity.java
@@ -22,7 +22,8 @@ public class MainActivity extends AppCompatActivity implements OnStreetViewPanor
 
     @Override
     public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
-        panorama.setPosition(new LatLng(-6.921932, 107.607639));
+        panorama.setPosition(new LatLng(-7.959662, 112.630695));
+        panorama.setStreetNamesEnabled(false);
         StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera.Builder()
                 .bearing(180)
                 .build();
